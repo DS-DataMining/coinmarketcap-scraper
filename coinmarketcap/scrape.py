c41531ac3046dd8cf3c9127136a9bad541617b4c
Lejla Kesko <lejla.kesko@hotmail.com>
2018-07-25 17:10:09 +0200

    Initial commit

--- coinmarketcap/scrape.py
+++ coinmarketcap/scrape.py
@@ -1,5 +1,3 @@
-import argparse
-import concurrent.futures
 import datetime
 
 import bs4
@@ -92,39 +90,3 @@ def str_to_date(s):
     return datetime.datetime.strptime(s, '%Y-%m-%d')
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--outfile', type=str)
-    parser.add_argument('--start', type=str_to_date, default=datetime.date(2013, 4, 28))
-    parser.add_argument('--end', type=str_to_date, default=datetime.date.today())
-    parser.add_argument('--symbols', type=str, nargs='*')
-    args = parser.parse_args()
-
-    all_df = parse_all_response(requests.get(all_url()))
-
-    slugs = all_df.slug.values
-    if args.symbols:
-        slugs = all_df.loc[all_df.symbol.isin(args.symbols)].slug.values
-    symbols = all_df.loc[all_df.slug.isin(slugs)].symbol.values
-
-    urls = [historical_coin_url(x, args.start, args.end) for x in slugs]
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        responses = [x for x in tqdm.tqdm(executor.map(requests.get, urls),
-                                          desc='downloading historical coin pages',
-                                          total=len(urls))]
-
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        historical_coin_dfs = [x for x in tqdm.tqdm(executor.map(parse_historical_coin_response, responses),
-                                                    desc='parsing historical coin pages',
-                                                    total=len(responses)) if x is not None]
-
-    for slug, symbol, historical_coin_df in zip(slugs, symbols, historical_coin_dfs):
-        historical_coin_df['slug'] = slug
-        historical_coin_df['symbol'] = symbol
-
-    pd.concat(historical_coin_dfs).to_csv(args.outfile)
-
-
-if __name__ == '__main__':
-    main()
